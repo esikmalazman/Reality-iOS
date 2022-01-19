cc16596622f3a2d96c88044e42e7240cac30d198
Ikmal Azman <opikmalazman@gmail.com>
2022-01-19 08:35:36 +0800

    setup configuration for image tracking

--- PentasReality/MainViewController.swift
+++ PentasReality/MainViewController.swift
@@ -10,55 +10,57 @@ import SceneKit
 import ARKit
 
 final class MainViewController: UIViewController {
-
+    //MARK: - Outlets
     @IBOutlet var sceneView: ARSCNView!
     
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
+        sceneView.autoenablesDefaultLighting = true
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        let asset = SCNScene(named: "art.scnassets/rumah-kampung.scn")!
+        sceneView.scene = asset
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
+        let configuration = ARImageTrackingConfiguration()
+        // Set number of image to track at once
+        configuration.maximumNumberOfTrackedImages = 1
+        guard let imagesToDetect = ARReferenceImage.referenceImages(inGroupNamed: "Pentas NFT", bundle: Bundle.main) else {
+            print("Could not find images to track at specify AR Resource Group")
+            return
+            
+        }
+        configuration.trackingImages = imagesToDetect
+        print("Image loaded")
         // Run the view's session
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
         // Pause the view's session
         sceneView.session.pause()
     }
-
+    
 }
 
 // MARK: - ARSCNViewDelegate
 extension MainViewController : ARSCNViewDelegate {
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
+    /*
+     // Override to create and configure nodes for anchors added to the view's session.
+     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+     let node = SCNNode()
      
-        return node
-    }
-*/
+     return node
+     }
+     */
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
